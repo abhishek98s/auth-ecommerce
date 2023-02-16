49b71faa6d8fe9573dbe6064bec50142eab3c49b
sth <sth>
2023-02-16 13:12:22 +0545

    added order components

---
+++ src/Components/Order/index.js
@@ -0,0 +1,47 @@
+import { useLocalStorage } from '@/Custom Hook/localStorage';
+import React from 'react'
+
+const Order = () => {
+
+    const [order, setOrder] = useLocalStorage("cart");
+
+    if (!order) {
+        return (
+            <>
+                <tr className="odd:bg-blue-600/[.2]">
+                    <td scope="row" className="px-6 py-3 w-full font-medium text-gray-900 dark:text-white">
+                        No orders are made
+                    </td>
+                    <td scope="row" className="px-6 py-3 w-full font-medium text-gray-900 dark:text-white">
+                        
+                    </td>
+                    <td scope="row" className="px-6 py-3 w-full font-medium text-gray-900 dark:text-white">
+                        
+                    </td>
+                </tr>
+            </>
+        )
+
+
+    }
+    return (
+        <>
+            {order.map((order) => (
+                <tr className="odd:bg-blue-600/[.2]">
+                    <th scope="row" className="px-6 py-3 w-full font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {order.title}
+                    </th>
+                    <td className="px-4 py-2 text-center">
+                        <span>{order.amount}</span>
+                    </td>
+                    <td className="px-4 py-2">
+                        <span>$ {order.price}</span>
+                    </td>
+                </tr>
+            ))
+            }
+        </>
+    )
+}
+
+export default Order
--- src/pages/order.js
+++ src/pages/order.js
@@ -1,15 +1,14 @@
 import { useLocalStorage } from '@/Custom Hook/localStorage';
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import OrderComponent from '@/Components/Order';
 
 const Order = () => {
   const orders = useSelector((state) => state.order.orders)
   const dispatch = useDispatch();
 
-  const [order, setOrder] = useLocalStorage("cart")
-
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-24 w-auto max-w-6xl mx-auto">
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-24 w-auto max-w-4xl mx-auto">
       <table className='w-full text-left' >
         <thead className="text-md text-gray-700 uppercase bg-blue-500/[.8]">
           <tr>
@@ -17,24 +16,15 @@ const Order = () => {
               Order name
             </th>
             <th className="px-6 py-5">
+              Amount
+            </th>
+            <th className="px-6 py-5">
               Price
             </th>
           </tr>
         </thead>
         <tbody>
-
-          {order.map((order) => (
-            <tr className="odd:bg-blue-600/[.2]">
-              <th scope="row" className="px-6 py-3 w-full font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {order.title}
-              </th>
-              <td className="px-4 py-2">
-                 <span>$ {order.price}</span>
-              </td>
-            </tr>
-          ))}
-
-
+          <OrderComponent />
         </tbody>
       </table>
     </div>
